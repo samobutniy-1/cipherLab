0a18b08ad0bcfd32242d8da82e89d32eb8e6fbde
webOwl <nikitaterentev916@gmail.com>
2026-04-14 16:52:15 +0200

    feat: create HomePage and Rain components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import { HomePage } from "./components/homepage/HomePage";
 
 function App() {
- 
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="bg-[#0a0a0f] w-full h-full flex items-center justify-center">
+      <HomePage />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/homepage/HomePage.tsx
@@ -0,0 +1,10 @@
+import { Rain } from "./Rain";
+
+export function HomePage() {
+  return (
+    <>
+      <Rain />
+      <h1 className="text-white text-5xl font-bold">cipherLab</h1>
+    </>
+  );
+}
