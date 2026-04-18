0c05fa7c65663c2f34b0c7e9798c72a3ecc89476
webOwl <nikitaterentev916@gmail.com>
2026-04-18 13:43:13 +0200

    feat: add React-Router to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { HomePage } from "./components/homepage/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
-    <div className="bg-[#0a0a0f] w-full h-full flex items-center justify-center">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="bg-[#0a0a0f] w-full h-full flex items-center justify-center">
+              <HomePage />
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/homepage/HomePage.tsx
+++ src/components/homepage/HomePage.tsx
@@ -1,7 +1,8 @@
 import { Rain } from "./Rain";
 import { ScrambleText } from "./ScrambleText";
+import { Link } from "react-router";
 
-const CHARS = ["ア", "∑", "Ψ", "0", "1", "!", "@", "#", "≠"];
+const CHARS = ["ア", "∑", "Ψ", "0", "1", "!", "@", "#", "≠", "カ"];
 
 function randChar() {
   return CHARS[Math.floor(Math.random() * CHARS.length)];
@@ -11,7 +12,15 @@ export function HomePage() {
   return (
     <>
       <Rain randChar={randChar} />
-      <ScrambleText text="cipherLab" randChar={randChar} />
+      <div className="flex flex-col justify-center items-center gap-10">
+        <ScrambleText text="cipherLab" randChar={randChar} />
+        <Link
+          className="text-white relative z-50 text-2xl bg-transparent border border-gray-600 rounded-xl p-3"
+          to="/ciphers"
+        >
+          explore ciphers
+        </Link>
+      </div>
     </>
   );
 }
